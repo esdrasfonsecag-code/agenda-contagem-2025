566fe8c16cca4a2c785057b53347babdd528112f
Esdras <esdrasfonsecag@gmail.com>
2025-11-27 10:12:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -36,38 +36,112 @@ const dias = [
     ]}
 ];
 
+// Lista completa de coordenadores e cursos
 const coordenadores = {
     "Ana Amelia Fonseca Viana Cruz": {
         email: "ana.vicruz@ulife.com.br",
-        cursos: ["Biomedicina","Biomedicina (Semipresencial) 30/70","Fisioterapia","Fisioterapia (Semipresencial) 30/70","Estética e Cosmética","Estética e Cosmética (Semipresencial) 30/70"]
+        cursos: [
+            "Biomedicina",
+            "Biomedicina (Semipresencial) 30/70",
+            "Fisioterapia",
+            "Fisioterapia (Semipresencial) 30/70",
+            "Estética e Cosmética",
+            "Estética e Cosmética (Semipresencial) 30/70"
+        ]
     },
     "Ana Paula dos Santos Gomes": {
         email: "ana.paula1@animaeducacao.com.br",
-        cursos: ["Nutrição","Nutrição (Semipresencial) 30/70","Psicologia"]
+        cursos: [
+            "Nutrição",
+            "Nutrição (Semipresencial) 30/70",
+            "Psicologia"
+        ]
     },
     "Camilla Ayala Felisberto Silva": {
         email: "camilla.felisberto@animaeducacao.com.br",
-        cursos: ["Análise e Desenvolvimento de Sistemas","Análise e Desenvolvimento de Sistemas (Semipresencial) 30/70","Ciência da Computação (Semipresencial) 30/70","Ciências da Computação","Direito","Sistemas de Informação","Sistemas de Informação (Semipresencial) 30/70"]
+        cursos: [
+            "Análise e Desenvolvimento de Sistemas",
+            "Análise e Desenvolvimento de Sistemas (Semipresencial) 30/70",
+            "Ciência da Computação (Semipresencial) 30/70",
+            "Ciências da Computação",
+            "Direito",
+            "Sistemas de Informação",
+            "Sistemas de Informação (Semipresencial) 30/70"
+        ]
     },
     "Cristina Carvalho de Melo": {
         email: "cristina.melo@animaeducacao.com.br",
-        cursos: ["Administração / Administração (Semipresencial) 30/70","Ciências Contábeis / Ciências Contábeis (Semipresencial) 30/70","Ciências Econômicas / Ciências Econômicas (Semipresencial) 30/70","Comércio Exterior","Educação física / Educação Física (Semipresencial) 30/70","Gestão Comercial","Gestão Comercial (Semipresencial) 30/70"]
+        cursos: [
+            "Administração / Administração (Semipresencial) 30/70",
+            "Ciências Contábeis / Ciências Contábeis (Semipresencial) 30/70",
+            "Ciências Econômicas / Ciências Econômicas (Semipresencial) 30/70",
+            "Comércio Exterior",
+            "Educação física / Educação Física (Semipresencial) 30/70",
+            "Gestão Comercial",
+            "Gestão Comercial (Semipresencial) 30/70",
+            "Gestão da Inovação e Empreendedorismo Digital",
+            "Gestão da Produção Industrial",
+            "Gestão da Qualidade",
+            "Gestão da Qualidade (Semipresencial) 30/70",
+            "Gestão da Tecnologia da Informação / Gestão da Tecnologia da Informação (Semipresencial) 30/70",
+            "Gestão de Processos e Projetos Ágeis",
+            "Gestão de Recursos Humanos / Gestão de Recursos Humanos (Semipresencial) 30/70",
+            "Gestão Financeira / Gestão Financeira (Semipresencial) 30/70",
+            "Gestão Pública (Semipresencial) 30/70",
+            "Governança Corporativa e Consultoria de Negócios",
+            "Investimento e formação de Traders",
+            "Logística / Logística (Semipresencial) 30/70",
+            "Marketing / Marketing (Semipresencial) 30/70",
+            "Marketing Digital",
+            "Negócios Imobiliários"
+        ]
     },
     "Daniel Portela Dias Machado": {
         email: "daniel.portela@animaeducacao.com.br",
-        cursos: ["Medicina Veterinária","Engenharia Civil","Engenharia Civil (Semipresencial) 30/70","Engenharia da Computação","Engenharia de Controle e Automação"]
+        cursos: [
+            "Medicina Veterinária",
+            "Engenharia Civil",
+            "Engenharia Civil (Semipresencial) 30/70",
+            "Engenharia da Computação",
+            "Engenharia de Controle e Automação",
+            "Engenharia de Produção",
+            "Engenharia de Produção (Semipresencial) 30/70",
+            "Engenharia de Software",
+            "Engenharia Elétrica",
+            "Engenharia Elétrica (Semipresencial) 30/70",
+            "Engenharia Mecânica",
+            "Engenharia Mecânica (Semipresencial) 30/70",
+            "Jornalismo",
+            "Publicidade e Propaganda"
+        ]
     },
     "Elisa Carvalho de Siqueira": {
         email: "elisa.siqueira@animaeducacao.com.br",
-        cursos: ["Odontologia","Design de Interiores","Design Gráfico","Design Gráfico (Semipresencial) 30/70"]
+        cursos: [
+            "Odontologia",
+            "Design de Interiores",
+            "Design Gráfico",
+            "Design Gráfico (Semipresencial) 30/70"
+        ]
     },
     "Elizabeth Rodrigues Brito Ibrahim": {
         email: "elizabeth.ibrahim@animaeducacao.com.br",
-        cursos: ["Arquitetura e Urbanismo","Farmácia","Farmácia (Semipresencial) 30/70","História","Pedagogia","Pedagogia (Semipresencial) 30/70"]
+        cursos: [
+            "Arquitetura e Urbanismo",
+            "Farmácia",
+            "Farmácia (Semipresencial) 30/70",
+            "História",
+            "Pedagogia",
+            "Pedagogia (Semipresencial) 30/70"
+        ]
     },
     "Marcos Ferreira Benedito": {
         email: "marcos.benedito@animaeducacao.com.br",
-        cursos: ["Enfermagem","Publicidade e Propaganda","Jornalismo"]
+        cursos: [
+            "Enfermagem",
+            "Publicidade e Propaganda",
+            "Jornalismo"
+        ]
     }
 };
 
@@ -120,4 +194,5 @@ document.getElementById("next").onclick = ()=>{
 
 atualizar();
 renderCoordenadores();
+
 };
